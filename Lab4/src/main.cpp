dfb27ff554839c56524158d3a3dde853b37bc285
Oliver <duqionl@gmail.com>
2023-03-30 15:13:35 -0700

    Update main.cpp
    adds main with logic

--- Lab4/src/main.cpp
+++ Lab4/src/main.cpp
@@ -18,6 +18,7 @@
 #include "pwm.h"
 #include "adc.h"
 #include "motor.h"
+#include "shiftRegister.h"
 
 #define MAX_OCR_VAL 1023
 
@@ -29,76 +30,68 @@ enum stateEnum {wait_press, debounce_press, wait_release, debounce_release};
 volatile stateEnum state = wait_press; //Initialize the state to waiting for button press
 
 
-//Tracks the current value displayed on the LEDS
-unsigned int currentVal = 0;
-unsigned int currentSpeedMultiplier = 1; //1 for normal, 2 for double speed
 
-volatile float currentVoltage = 0;
 
-int main(){
-  //enable interrupts
-  sei();
-  //init needed components
-  initSwitchPB3();
+int main() {
+  initializeADC();
+  initPWMTimer3();
   initTimer0();
   initTimer1();
-  initLCD();
-  initPWMTimer3();
-  initializeADC();
 
- while (1) {
+  Serial.begin(9600);
+  unsigned int result = 0;
+  double voltage = 0;
+  int cycleVal = 0;
+  while(1){
 
+  switch(state) {
+    case wait_press:
+        result = ADCL;
+        result = (((unsigned int) ADCH) << 8) + result;
+        voltage = result * (5/1024.0);
+        Serial.println(voltage,2);
 
-  //outer state machine for button logic
-
-  //Get current ADC voltage
-  currentVoltage = (((ADCH << 8) | ADCL) / MAX_OCR_VAL) * 5.0;
-  //HANDLE MOTOR CONTROL LOGIC
-  if (currentVoltage < 2.5) {
-    //Set desired motor speed
-    changeDutyCycle((currentVoltage / 2.5) * MAX_OCR_VAL);
-    //Set correct motor direction. IDK if forward corresponds to CW or CCW, we can flip later if wrong
-    motorDirection(1);
+        if (voltage <= 2.5) {
+          //Set desired motor speed
+          cycleVal = (1 - (voltage / 2.5)) * MAX_OCR_VAL;
+          Serial.println(cycleVal);
+          changeDutyCycle(cycleVal);
+          //Set correct motor direction. IDK if forward corresponds to CW or CCW, we can flip later if wrong
+          motorDirection(1);
+        }
+        else {
+          //Set desired motor speed
+          cycleVal = ((voltage - 2.5) / 2.5) * MAX_OCR_VAL;
+          Serial.println(cycleVal);
+          changeDutyCycle(cycleVal);
+          //Set correct motor direction. IDK if backward corresponds to CW or CCW, we can flip later if wrong
+          motorDirection(2);
+        }
+      break;
+    case debounce_press:
+      delayUs(1); //Adds delay to account for debounce period
+      state = wait_release;
+      break;
+    case wait_release: 
+     break;
+    case debounce_release: //Add delay to account for debounce period
+      delayUs(1);
+      tenSecTimerCountdownDisplay();
+      state = wait_press;
+      break;
   }
-  else if (currentVoltage == 2.5) {
-    //Turn motor off
-    changeDutyCycle(0);
   }
-  else {
-    //Set desired motor speed
-    changeDutyCycle(((currentVoltage - 2.5) / 2.5) * MAX_OCR_VAL);
-    //Set correct motor direction. IDK if backward corresponds to CW or CCW, we can flip later if wrong
-    motorDirection(2);
-  }
-
-//   switch(state) {
-//     case wait_press:
-//       break;
-//     case debounce_press:
-//       delayUs(1); //Adds delay to account for debounce period
-//       state = wait_release;
-//       break;
-//     case wait_release: 
-//      break;
-//     case debounce_release: //Add delay to account for debounce period
-//       delayUs(1);
-//       state = wait_press;
-//       break;
-//   }
-//  }
 }
 
-/* Implement an Pin Change Interrupt which handles the switch being
-* pressed and released. When the switch is pressed and released, the LEDs
-* change at twice the original rate. If the LEDs are already changing at twice
-* the original rate, it goes back to the original rate.
-*/
-// register that corresponds to port b, which is the port our button is on
-// ISR(PCINT0_vect){ //On interrupt, advance state machine
-//   if (state == wait_press){
-//     state = debounce_press;
-//   }
-//   else if (state == wait_release) {
-//     state = debounce_release;
-//   }
+  // register that corresponds to port b, which is the port our button is on
+ISR(PCINT0_vect){ //On interrupt, advance state machine
+  if (state == wait_press){
+    state = debounce_press;
+  }
+  else if (state == wait_release) {
+    state = debounce_release;
+  }
 }
+
+
+
