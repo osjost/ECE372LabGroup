585f94567ae0cc7fa10f12ccd31410a5cfa75d11
riosnabor <112442726+riosnabor@users.noreply.github.com>
2023-04-20 14:40:30 -0700

    Update i2c.cpp

--- Lab5/src/i2c.cpp
+++ Lab5/src/i2c.cpp
@@ -10,7 +10,7 @@ void initI2C() {
   PRR0 &= ~(1 << PRTWI);
   TWSR &= ~((1 << TWPS0) | (1 << TWPS1)); //set prescaler to 1
   
-  TWBR = 8; //setting TWBR to 8 to satisfy SCL freq requirement
+  TWBR = 3; //setting TWBR to 3 to satisfy SCL freq requirement
 
   TWCR |= (1 << TWINT) | (1 << TWEN);
 }
@@ -18,33 +18,84 @@ void initI2C() {
 void StartI2C_Trans(unsigned char SLA) {
   TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN); //clear TWINT, start condition, and enable
   wait_for_completion;
+   
+
   TWDR = (SLA << 1); //write bit is 0, so shift left sets write bit to 0
-  TWCR = (1 << TWINT) | (1 << TWEN); // clear falg and enable TWI
+  TWCR = (1 << TWINT) | (1 << TWEN); // clear flag and enable TWI
   wait_for_completion;
+ 
 }
 
 void StopI2C_Trans() {
   TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
 }
 
-Read_from(unsigned char SLA, unsigned char MEMADDRESS) {
-  StartI2C_Trans(SLA);
+void Read_from(unsigned char SLA, unsigned char MEMADDRESS) {
+  //StartI2C_Trans(SLA);
 
-  TWDR = MEMADDRESS;
+   signed int x=1;
+    write(MEMADDRESS); //nternal mem address
+  
+
+  TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN); //restart for read mode
+  wait_for_completion;
+//   Serial.println(TWSR & (0xF8),HEX); // print out status register to verify transmission
+//   Serial.flush();
+
+  TWDR = (SLA << 1) | READ_BIT;         //7bit addy with read bit
   TWCR = (1 << TWINT) | (1 << TWEN);
   wait_for_completion;
+//   Serial.println(TWSR & (0xF8),HEX); // print out status register to verify transmission
+//   Serial.flush();
 
-  TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-  wait_for_completion;
 
-  TWDR = (1 << TWINT) | 0x01;
-  TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+
+  TWCR = (1 << TWINT) | (1 << TWEN) | (1<< TWEA); // trigger with master sending ack
   wait_for_completion;
-  TWCR = (1 << TWINT) | (1 << TWEN);
+  x = TWDR;     //lower bit for x
+  TWCR = (1 << TWINT) | (1 << TWEN)| (1 << TWEA);// trigger with master sending ack
   wait_for_completion;
-  TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+  x = (TWDR << 8) | x;  // high byte for x axis
+
+  Serial.println("x = ");
+  Serial.print(x);
+
+
+
+//   TWCR = (1 << TWINT) | (1 << TWEN) | (1<<TWEA); // trigger with master sending ack
+//   wait_for_completion;
+//   y = TWDR;     //lower bit for y
+//   TWCR = (1 << TWINT) | (1 << TWEN)| (1 << TWEA);// trigger with master sending ack
+//   wait_for_completion;
+//   y = (TWDR << 8) | y;  // high byte for y axis
+
+//   Serial.println("y = ");
+//   Serial.print(y);
 
   
+
+
+//   TWCR = (1 << TWINT) | (1 << TWEN) | (1<<TWEA); // trigger with master sending ack
+//   wait_for_completion;
+//   z = TWDR;     //lower bit for z
+//   TWCR = (1 << TWINT) | (1 << TWEN)| (1 << TWEA);// trigger with master sending ack
+//   wait_for_completion;
+//   z = (TWDR << 8) | z;  // high byte for z axis
+
+//   Serial.println("z = ");
+//   Serial.print(z);
+
+
+//    Serial.println(TWSR & (0xF8),HEX); // print out status register to verify transmission
+//    Serial.flush();
+
+}
+
+void write(unsigned char data){
+  // this function loads the data passed into the I2C data register and transmits
+  TWDR = data; //load data into TWDR register
+  TWCR = (1<<TWINT)|(1<<TWEN);  // trigger action:  clear flag and enable TWI
+  wait_for_completion;
 }
 
 unsigned char Read_data() {
