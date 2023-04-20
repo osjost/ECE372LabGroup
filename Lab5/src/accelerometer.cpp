ce642acd76349d331fd04d1acd50312242f32173
riosnabor <112442726+riosnabor@users.noreply.github.com>
2023-04-20 14:39:14 -0700

    Update accelerometer.cpp

--- Lab5/src/accelerometer.cpp
+++ Lab5/src/accelerometer.cpp
@@ -1,6 +1,7 @@
 #include "accelerometer.h"
 #include "i2c.h"
 #include <avr/io.h>
+#include "Arduino.h"
 
 #define wait_for_completion while(!(TWCR & (1 << TWINT)));
 #define WRITE_BIT 0;
@@ -13,7 +14,7 @@
 #define MPU_YOUT_L 0x3E
 #define MPU_ZOUT_H 0x3F
 #define MPU_ZOUT_L 0x40
-#define MPU_WHO_AM_I 0x75        //I2C addy
+#define MPU_WHO_AM_I 0x68        //I2C addy
 #define MPU_PWR_MANAGEMENT_1_REG 0x6B  
 #define MPU_PWR_CONFIG  0x09 //sets internal accel clock to X-gyro for improved stability (datasheet recommended) 
                              //disables temp sensor (not needed for this lab)
@@ -24,17 +25,17 @@
 void initAccelerometer() {
 
     StartI2C_Trans(MPU_WHO_AM_I);
+    //  Serial.println(TWSR & (0xF8),HEX); // print out status register to verify transmission
+    //  Serial.flush();
 
     write(MPU_PWR_MANAGEMENT_1_REG);
+    // Serial.println(TWSR & (0xF8),HEX); // print out status register to verify transmission
+    // Serial.flush();
 
     write(MPU_PWR_CONFIG);
+    // Serial.println(TWSR & (0xF8),HEX); // print out status register to verify transmission
+    // Serial.flush();
 
 
 }
 
-void write(unsigned char data){
-  // this function loads the data passed into the I2C data register and transmits
-  TWDR = data; //load data into TWDR register
-  TWCR = (1<<TWINT)|(1<<TWEN);  // trigger action:  clear flag and enable TWI
-  wait_for_completion;
-}
