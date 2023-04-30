ed1bbe55e04808ef1b1f7f137bf25ba363937f87
riosnabor <112442726+riosnabor@users.noreply.github.com>
2023-04-30 16:39:27 -0700

    Create ad1115.cpp

---
+++ Final Project/src/ad1115.cpp
@@ -0,0 +1,29 @@
+#include "ad1115.h"
+#include <avr/io.h>
+#include <Arduino.h>
+#include "i2c.h"
+
+#define wait_for_completion while(!(TWCR & (1 << TWINT)));
+#define WRITE_BIT 0;
+#define READ_BIT 1;
+
+#define ADD1115_ADDY 0b1001000
+
+
+void initAD1115() {         //sets the module to continous conversion mode
+
+    StartI2C_Trans(ADD1115_ADDY);
+
+
+
+    write(0b00000001);      //points to the config register of module
+
+
+
+    write(0b10000100); //msbyte of config register to be written. 
+
+   
+
+    write(0b10000011); //lsbyte of config register to be written
+
+}
