9d3755a2115ebd4bf472d667b92c2b646caa1dbc
riosnabor <112442726+riosnabor@users.noreply.github.com>
2023-04-30 16:40:58 -0700

    Create i2c.h

---
+++ Final Project/src/i2c.h
@@ -0,0 +1,19 @@
+#ifndef I2C_H
+#define I2C_H
+
+void initI2C();
+
+void StartI2C_Trans(unsigned char SLA);
+
+void StopI2C_Trans();
+
+void Read_from(unsigned char SLA, unsigned char MEMADDRESS);
+
+void Read_from_16bit(unsigned char SLA, unsigned char MEMADDRESS);
+
+
+unsigned char Read_data();
+
+void write(unsigned char);
+
+#endif
